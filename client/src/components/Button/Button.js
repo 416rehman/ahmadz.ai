128423111aa27adac4ca1f861ca5afccb0c8c57e
Rehman <certifiedrehman@gmail.com>
2021-09-19 18:46:28 -0400

    Removed redundant backend. Blogs are handled by another project

--- client/src/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import './Button.css'
-export default function Button(props) {
-    return(<a className={'Button'} href={props.href || props.url} target={`${props.target}`}>
-        <div>{props.text || props.value}</div>
-        {props.icon}
-    </a>)
-}
