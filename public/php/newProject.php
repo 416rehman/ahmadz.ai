79e83a74d195620b22227ed95c03600e7dd214dc
Rehman <rehman@ahmadz.ai>
2022-01-17 19:17:48 +0000

    updated docker strategy

--- public/php/newProject.php
+++ public/php/newProject.php
@@ -50,13 +50,11 @@ if (isset($_SESSION['id']) && $_SESSION['id'] == $authorized_github_id) {
         )
     );
 
-    print_r($newProject);
-
     # add the new project to the top of the array
     array_unshift($projects, $newProject);
 
     $projects = json_encode($projects, JSON_PRETTY_PRINT);
-    print_r(file_put_contents("../data/projects.json", $projects));
+    file_put_contents("../data/projects.json", $projects);
 
    header("Location: /#projects&success");
 
